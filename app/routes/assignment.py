e846be9eea8693c9fc921dc0536190247b630ee2
YoonDaesung <yds6760@naver.com>
2025-04-08 00:39:55 +0900

    퀴즈 기능

--- app/routes/assignment.py
+++ app/routes/assignment.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.models import Assignment
 from app.schemas import AssignmentCreate, AssignmentOut
-from database import get_db
+from app.database import get_db
 
 router = APIRouter()
 
