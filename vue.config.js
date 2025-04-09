2e3c0c5157acbaaf3ee34561b056d2d262bb2255
kmmnj-iiu <rlaalswn5713@koreatech.ac.kr>
2025-04-09 15:48:48 +0900

    Fix: F5

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: '/',
   transpileDependencies: true,
   chainWebpack: config => {
     config.plugin('define').tap(definitions => {
