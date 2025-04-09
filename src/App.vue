2e514849a89ed0b0bb9e5118892faa41aba985de
gayeon0815 <gayeon0815@naver.com>
2025-04-09 13:10:26 +0900

    오류 수정

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@
     <!-- 현재 경로가 '/'가 아닐 때만 네비게이션 바 표시 -->
     <NavbarComponent v-if="!isHome" />
 
-    <!-- ✅ 여기에 TestApi 추가 (테스트 후 나중에 지워도 됨) -->
-    <TestApi />
-
     <!-- 라우터 뷰는 항상 표시 -->
     <div>
       <router-view></router-view>
@@ -17,12 +14,10 @@
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 import NavbarComponent from './components/NavbarComponent.vue'
-import TestApi from './views/TestApi.vue' // ✅ TestApi 임포트 추가
 
 export default {
   components: {
     NavbarComponent,
-    TestApi, // ✅ 등록
   },
   setup() {
     const route = useRoute()
@@ -33,3 +28,4 @@ export default {
   },
 }
 </script>
+
