9cb42e2ed51ef7f7f6002b77716dc1fff267179f
YoonDaesung <yds6760@naver.com>
2025-04-11 19:24:07 +0900

    짜증나니까

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 # 환경 변수에서 값 불러오기
-ASSISTANT_ID = os.getenv("ASSISTANT_ID")
+ASSISTANT_ID = os.getenv("OPENAI_ASSISTANT_ID")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # 환경 변수가 설정되지 않은 경우 에러 처리 (선택 사항)
