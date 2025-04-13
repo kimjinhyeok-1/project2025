2c882e3e118c4d56e837c66870eabdbbd824e28e
YoonDaesung <yds6760@naver.com>
2025-04-13 19:43:43 +0900

    테스트

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_ASSISTANT_ID = os.getenv("OPENAI_ASSISTANT_ID")
 
 if not OPENAI_API_KEY:
-    raise ValueError("OPENAI_API_KEY 환경 변수가 없습니다.")
+    raise ValueError("OPENAI_API_KEY 환경변수가 없습니다.")
 if not OPENAI_ASSISTANT_ID:
-    raise ValueError("OPENAI_ASSISTANT_ID 환경 변수가 없습니다.")
+    raise ValueError("OPENAI_ASSISTANT_ID 환경변수가 없습니다.")
