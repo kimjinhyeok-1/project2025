82a9d115dd504c995497c3192a3c12cd0300b00a
kmmnj-iiu <rlaalswn5713@koreatech.ac.kr>
2025-04-27 18:21:57 +0900

    2025.04.27 18:21

--- src/router/index.js
+++ src/router/index.js
@@ -4,22 +4,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 // 학생 관련 뷰
-import StudentLayout from '../views/StudentLayout.vue'
-import StudentQnA from '../views/StudentQnA.vue'
-import StudentAssignment from '../views/StudentAssignment.vue'
+import StudentLayout from '../views/Stu_Layout.vue'
+import StudentQnA from '../views/Stu_AIQnAprof.vue'
+import StudentAssignment from '../views/Stu_Assign.vue'
 import StudentHistory from '../views/StudentHistory.vue'
 
 // 교수 관련 뷰
-import ProfessorLayout from '../views/ProfessorLayout.vue'
-import ProfessorView from '../views/ProfessorView.vue'
-import ProfessorLesson from '../views/ProfessorLesson.vue'
-import ProfessorQnA from '../views/ProfessorQnA.vue'
-import ProfessorAssignments from '../views/ProfessorAssignments.vue'
-import ProfessorReviewView from '../views/ProfessorReviewView.vue'
-import ProfessorSummaryDetailView from '../views/ProfessorSummaryDetailView.vue'
+import ProfessorLayout from '../views/Prof_Layout.vue'
+import ProfessorView from '../views/Prof_DashBoard.vue'
+import ProfessorLesson from '../views/Prof_Lesson.vue'
+import ProfessorQnA from '../views/Prof_QnA.vue'
+import ProfessorAssignments from '../views/Prof_Assign.vue'
+import ProfessorReviewView from '../views/Prof_Review.vue'
+import ProfessorSummaryDetailView from '../views/Prof_SummaryDetail.vue'
 import AssignmentPostForm from '../views/AssignmentPostForm.vue'
 import AssignmentSubmit from '../views/AssignmentSubmit.vue'
-import ProfessorRealtimeQuestion from '../views/ProfessorRealtimeQuestion.vue'
+import ProfessorRealtimeQuestion from '../views/Prof_AIQnAStu.vue'
 
 const routes = [
   {
