ca6cde0abd5fbd251aaade8b88a1f468619d91f8
kmmnj-iiu <rlaalswn5713@koreatech.ac.kr>
2025-05-05 21:40:59 +0900

    2025.05.05 21:40

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,6 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: ['@babel/plugin-transform-private-methods'],
+  plugins: ['@babel/plugin-transform-private-methods',
+    '@babel/plugin-transform-class-static-block',],
 }
