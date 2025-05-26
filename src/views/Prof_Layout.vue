ce8272337ebc7fb0026d6786ca199106ac9959a9
kmmnj-iiu <rlaalswn5713@koreatech.ac.kr>
2025-05-27 00:13:32 +0900

    2025.05.27 00:13

--- src/views/Prof_Layout.vue
+++ src/views/Prof_Layout.vue
@@ -11,15 +11,15 @@
 
       <hr class="sidebar-divider my-0" />
 
-      <li class="nav-item my-3">
+      <li class="nav-item">
         <router-link class="nav-link" to="/professor/qna">
           <i class="fas fa-question-circle"></i>
-          <span>학생들의 질문</span>
+          <span> 학생들의 질문</span>
         </router-link>
       </li>
 
       <!-- ✅ 드롭다운 메뉴 시작 -->
-      <li class="nav-item my-3">
+      <li class="nav-item">
         <a
           class="nav-link collapsed"
           href="#"
@@ -29,21 +29,21 @@
           aria-controls="collapseLesson"
         >
           <i class="fas fa-chalkboard"></i>
-          <span>수업</span>
+          <span> 수업</span>
         </a>
         <div id="collapseLesson" class="collapse" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded text-center">
-            <router-link class="collapse-item my-1" to="/professor/lesson">수업 시작</router-link>
-            <router-link class="collapse-item my-1" to="/professor/review">수업 요약</router-link>
+          <div class="bg-white py-2 collapse-inner rounded">
+            <router-link class="collapse-item" to="/professor/lesson">수업 시작</router-link>
+            <router-link class="collapse-item" to="/professor/review">수업 요약</router-link>
           </div>
         </div>
       </li>
       <!-- ✅ 드롭다운 끝 -->
 
-      <li class="nav-item my-3">
+      <li class="nav-item">
         <router-link class="nav-link" to="/professor/assignments">
           <i class="fas fa-tasks"></i>
-          <span>과제</span>
+          <span> 과제</span>
         </router-link>
       </li>
     </ul>
@@ -58,33 +58,8 @@
 </template>
 
 <style scoped>
-.sidebar .nav-link {
-  display: flex !important;
-  flex-direction: column !important;
-  align-items: center !important;
-  text-align: center !important;
-}
-
-.sidebar .nav-link i {
-  font-size: 1.6rem !important; /* ⬅️ 아이콘 크기 확실히 키우기 */
-  margin-bottom: 6px;
-  line-height: 1;
-  display: block;
-}
-
 .sidebar .nav-link span {
-  font-size: 1.1rem;
+  font-size: 1.2rem;
   font-weight: 600;
 }
-
-.nav-item {
-  margin-top: 0.5rem !important;
-  margin-bottom: 0.5rem !important;
-}
-
-.collapse-inner .collapse-item {
-  font-size: 1rem;
-  font-weight: 500;
-}
 </style>
-
